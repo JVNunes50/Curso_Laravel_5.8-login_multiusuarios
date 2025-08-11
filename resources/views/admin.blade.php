367a5302ba5d2d9b774d77455a110932ba0a441d
JVNunes50 <jvnunes1944@gmail.com>
2025-08-11 20:48:30 -0300

    Criando Controlador e Views

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard do Usuario</div>
+                <div class="card-header">Dashboard do ADM</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    Voce esta logado como usuario.
+                    Voce esta logado como Adiministrador.
                 </div>
             </div>
         </div>
