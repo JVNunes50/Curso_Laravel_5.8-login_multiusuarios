8f75ffe8ef3750ce3896e100b074d71c714fa640
JVNunes50 <jvnunes1944@gmail.com>
2025-08-11 21:12:53 -0300

    Ajuste da rota

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/home', 'AdminController@index')->name('homeadmin');
+Route::get('/admin', 'AdminController@index')->name('homeadmin');
