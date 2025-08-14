ebe2aefaaf2564d32030ae97554ca91405074dc5
JVNunes50 <jvnunes1944@gmail.com>
2025-08-13 21:41:32 -0300

    Implementando verificação do login

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class AdminLoginController extends Controller
 {
@@ -13,7 +14,23 @@ class AdminLoginController extends Controller
     }
 
     public function login(Request $request){
-        return "Ok";
+        $request->validate([
+            $this->username() => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $credenciais = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        $authOK = Auth::guard('admin')->attempt($credenciais, $request->remember);
+
+        if ($authOK) {
+            return redirect()->intended(route('admin.dashboard'));
+        }
+        
+        return redirect()->back()->withInput($request->only('email', 'remember'));
     }
 
     public function index(){
