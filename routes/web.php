9727eb123b2c683fd9afd08c1df68c0266affcd6
JVNunes50 <jvnunes1944@gmail.com>
2025-08-13 21:19:48 -0300

    Testando Login Admin

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
 
-Route::get('/admin/login', 'AdminController@index')->name('admin.login');
 
-Route::post('/admin/login', 'AdminController@index')->name('admin.login.submit');
+Route::get('/admin/login', 'Auth\AdminLoginController@index')->name('admin.login');
+
+Route::post('/admin/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
